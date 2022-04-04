42b7d06863c48b5119b5128696e4a26ec2cdd5dc
Herman Andres <heanfig@gmail.com>
2022-04-04 11:29:19 -0500

    feat(general): Add Angular Material deps

---
+++ libs/ui/src/lib/ui.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonComponent } from './button/button.component';
+import { InputComponent } from './input/input.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [
+    ButtonComponent,
+    InputComponent
+  ],
+})
+export class UiModule {}
